adfdaf24b3c289fc55cc84f950ee45026b1c016d
Elisha David <kingdaviinchi@gmail.com>
2024-09-23 23:55:32 +0100

    Created a component solving cat and the office I-Outed  from code wars

---
+++ theOfficeIOuted.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/solutions/javascript
+
+function outed(meet, boss) {
+  let names = Object.keys(meet);
+  let score = names.reduce((s, v) => s + meet[v], 0) + meet[boss];
+  return score / names.length > 5 ? "Nice Work Champ!" : "Get Out Now!";
+}
