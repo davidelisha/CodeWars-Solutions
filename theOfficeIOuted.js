222c3bb9dc949059cb6406fd41dba1522ecb08a3
Elisha David <kingdaviinchi@gmail.com>
2024-09-24 00:08:41 +0100

    Created a component solving convert the score from code wars

--- theOfficeIOuted.js
+++ theOfficeIOuted.js
@@ -1,7 +1,32 @@
 //https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/solutions/javascript
 
-function outed(meet, boss) {
-  let names = Object.keys(meet);
-  let score = names.reduce((s, v) => s + meet[v], 0) + meet[boss];
-  return score / names.length > 5 ? "Nice Work Champ!" : "Get Out Now!";
+function scoreboard(string) {
+  const numberWords = {
+    nil: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  let words = string.toLowerCase().split(" ");
+
+  let result = [];
+
+  for (let word of words) {
+    if (numberWords.hasOwnProperty(word)) {
+      result.push(numberWords[word]);
+    }
+
+    if (result.length === 2) {
+      break;
+    }
+  }
+
+  return result;
 }
